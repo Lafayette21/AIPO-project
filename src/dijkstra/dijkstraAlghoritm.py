ff848c05043292b8c569d9f2285b7cf37b33b26f
kinan11 <kingamiszczak1@op.pl>
2023-06-22 22:44:36 +0200

    Wybór puktu docelowego

--- src/dijkstra/dijkstraAlghoritm.py
+++ src/dijkstra/dijkstraAlghoritm.py
@@ -1,7 +1,6 @@
 import sys
 
-
-def dijkstra(graph, start):
+def dijkstra(graph, start, target):
     distances = {vertex: sys.maxsize for vertex in graph}
     distances[start] = 0
     visited = []
@@ -17,16 +16,17 @@ def dijkstra(graph, start):
 
         visited.append(min_vertex)
 
+        if min_vertex == target:
+            break
+
         if min_vertex is not None:
             for neighbor, weight in graph[min_vertex].items():
                 distance = distances[min_vertex] + weight
                 if distance < distances[neighbor]:
                     distances[neighbor] = distance
 
-    return distances
+    return distances[target]
 
-
-# Graf w postaci słownika, gdzie klucze to wierzchołki, a wartości to słowniki reprezentujące sąsiadów i wagi krawędzi
 graph = {
     'A': {'B': 2, 'D': 4},
     'B': {'C': 3, 'D': 3},
@@ -36,10 +36,10 @@ graph = {
 }
 
 start_vertex = 'C'
-distances = dijkstra(graph, start_vertex)
+target_vertex = 'B'
+shortest_distance = dijkstra(graph, start_vertex, target_vertex)
 
-# Wyświetlenie wyników
-for vertex, distance in distances.items():
-    if distance == sys.maxsize:
-        distance = None
-    print(f'Najkrótsza odległość z wierzchołka {start_vertex} do wierzchołka {vertex}: {distance}')
+if shortest_distance == sys.maxsize:
+    shortest_distance = None
+
+print(f'Najkrótsza odległość z wierzchołka {start_vertex} do wierzchołka {target_vertex}: {shortest_distance}')
