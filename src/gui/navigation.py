4e75d53106edbeb06e41c53fbd65c08156f69d1d
Marcin Urbanowicz <thmarcin@student.agh.edu.pl>
2023-07-03 20:00:36 +0200

    Refactor Navigation class

--- src/gui/navigation.py
+++ src/gui/navigation.py
@@ -1,30 +1,34 @@
-
 import cv2
 from PIL import Image, ImageDraw
 
 from binarizer.binarizer import Binarizer
 
+from .point import Point
+
 
 class Navigation:
+    POINT_RADIUS: int = 10
+
     def __init__(self):
         self.binarizer = Binarizer()
 
-    def navigate(self, image, startPoint, endPoint):
-        binarized_image = self.binarizer.binarize(image)
-        output_image_pil = self.convert_to_pil(binarized_image)
-        
-        draw = ImageDraw.Draw(output_image_pil)
+    def navigate(self, image, start_point: Point, end_point: Point):
+        binary_image = self.binarizer.binarize(image)
+        output_image_pil = self._convert_to_pil(binary_image)
 
-        r = 10
-        x, y = startPoint.x, startPoint.y
-        draw.ellipse((x-r, y-r, x+r, y+r), fill='red')
+        drawer = ImageDraw.Draw(output_image_pil)
 
-        x, y = endPoint.x, endPoint.y
-        draw.ellipse((x-r, y-r, x+r, y+r), fill='red')
+        self._draw_point(start_point, drawer)
+        self._draw_point(start_point, drawer)
 
         return output_image_pil
 
-    def convert_to_pil(self, image):
+    def _convert_to_pil(self, image):
         image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         image_pil = Image.fromarray(image_rgb)
         return image_pil
+
+    def _draw_point(self, point: Point, drawer: ImageDraw):
+        drawer.ellipse(point.x - self.POINT_RADIUS, point.y - self.POINT_RADIUS,
+                       point.x + self.POINT_RADIUS, point.y + self.POINT_RADIUS,
+                       fill='red')
