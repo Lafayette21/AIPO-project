efa042db9f9e27e8f0feecc54b71637b8095eb53
Marcin Urbanowicz <thmarcin@student.agh.edu.pl>
2023-07-03 21:54:44 +0200

    Make main function change window parameters

--- src/main.py
+++ src/main.py
@@ -3,6 +3,6 @@ from src.gui.application import Application
 if __name__ == "__main__":
     app = Application()
     app.title("GPS application")
-    app.geometry("1000x800")
-    app.initialise()
+    app.geometry("1024x600")
+    app.resizable(width=True, height=True)
     app.mainloop()
