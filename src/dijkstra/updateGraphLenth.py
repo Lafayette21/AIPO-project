f86f88b6a6d3ab5edf865b9f5bf8d0622a344dc7
kinan11 <kingamiszczak1@op.pl>
2023-07-04 15:17:39 +0200

    Ostateczne poprawki

--- src/dijkstra/updateGraphLenth.py
+++ src/dijkstra/updateGraphLenth.py
@@ -2,5 +2,8 @@ def update_edge_lengths(graph):
     for u, v in graph.edges:
         edge_length = graph[u][v]['length']
         edge_area = graph[u][v]['area']
-        updated_length = edge_length / (0.1 * edge_area / (edge_length+1)) # TODO : Do something if length 0 temporary added 1
+        if edge_length == 0:
+            updated_length = 0
+        else:
+            updated_length = edge_length / (0.1 * edge_area / (edge_length+1))
         graph[u][v]['length'] = updated_length
